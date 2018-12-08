5b6b77050cdd9dd03cf30971c66d95d4a6c3ed6e
vaskiv99 <vaskiv99@ukr.net>
2018-12-08 11:18:14 +0200

    Fix deploy to docker hub.

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
+if [ "$TRAVIS_BRANCH" == "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
